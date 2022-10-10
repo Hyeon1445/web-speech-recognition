a29775ec71efec7aa45578c3d75ceee1f4b4be93
Hyeon1445 <lily1445@gmail.com>
2022-10-10 19:40:38 +0900

    feat: add speak button

--- components/speech-recognition/index.tsx
+++ components/speech-recognition/index.tsx
@@ -1,51 +1,49 @@
 import RecognizedText from '@components/recognized-text'
 import styled from '@emotion/styled'
 import { Canvas } from '@react-three/fiber'
-import { Dispatch, SetStateAction, useState } from 'react'
-
-const handleSpeech = (setText: Dispatch<SetStateAction<string>>) => {
-  const Recognition =
-    //@ts-ignore
-    window.SpeechRecognition || window.webkitSpeechRecognition
-  if (!Recognition) {
-    console.log('Speech Recognition is not support in this browser')
-  }
-  const speechToText = new Recognition()
-  speechToText.lang = 'ko-KR'
-  speechToText.continuous = false
-  speechToText.intermResults = false
-  speechToText.maxAlternatives = 1
-  speechToText.onresult = (event: any) => {
-    const text = event.results[0][0].transcript
-    console.log(text)
-    setText(text.split(' ').join('\n'))
-  }
-  // speechToText.onspeechend = () => {
-  //   // speech end
-  // }
-  speechToText.onnomatch = () => {
-    console.log('Sorry')
-  }
-  speechToText.onstart = () => {
-    setText('Listening...')
-  }
-  // speechToText.onend = () => {
-  //   // speech end
-  //   console.log('on end')
-  // }
-  speechToText.onerror = () => {
-    console.log('on error')
-    setText('...')
-  }
-  speechToText.start()
-}
+import { useState } from 'react'
 
 const SpeechRecognition = () => {
   const [text, setText] = useState('...')
+  const [isListening, setIsListening] = useState(false)
+
+  const handleSpeech = () => {
+    const Recognition =
+      //@ts-ignore
+      window.SpeechRecognition || window.webkitSpeechRecognition
+    if (!Recognition) {
+      const warningText = 'This browser does not support speech recognition'
+      setText(warningText.split(' ').join('\n'))
+      return
+    }
+    const speechToText = new Recognition()
+    speechToText.lang = 'ko-KR'
+    speechToText.continuous = false
+    speechToText.intermResults = false
+    speechToText.maxAlternatives = 1
+    speechToText.onresult = (event: any) => {
+      const text = event.results[0][0].transcript
+      console.log(text)
+      setText(text.split(' ').join('\n'))
+    }
+    speechToText.onstart = () => {
+      setText('Listening...')
+      setIsListening(true)
+    }
+    speechToText.onend = () => {
+      setIsListening(false)
+      if (text === 'Listening...') {
+        setText('...')
+      }
+    }
+    speechToText.onerror = () => {
+      setText('...')
+    }
+    speechToText.start()
+  }
 
   return (
     <div>
-      <button onClick={() => handleSpeech(setText)}>speak</button>
       <Canvas
         camera={{
           fov: 45,
@@ -60,8 +58,22 @@ const SpeechRecognition = () => {
       >
         <RecognizedText text={text} />
       </Canvas>
+      {!isListening && (
+        <SpeakButton className="material-icons md-36" onClick={handleSpeech}>
+          mic
+        </SpeakButton>
+      )}
     </div>
   )
 }
 
+const SpeakButton = styled.button`
+  background-color: #e2ffa7;
+  position: absolute;
+  bottom: 20vh;
+  height: 3rem;
+  width: 3rem;
+  border-radius: 99px;
+  left: calc(50% - 1.5rem);
+`
 export default SpeechRecognition
